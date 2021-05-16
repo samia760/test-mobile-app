1ca47c7a87bb6342568411f20bf1e49e7cef7047
Samia Shakeel <Samiashakeel0342@gmail.com>
2021-05-16 07:58:36 -0700

    work Accomplish

---
+++ android/app/src/main/kotlin/com/example/samiashakeel/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.samiashakeel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+ class MyApp extends StatelessWidget {
+   @override
+   Widget build(BuildContext context) {
+     return MaterialApp(
+       home:(
+           Scaffold(body: Text("Name:Samia Shakeel Email:Samiashakeel0342@gmail.com"))
+       ),
+     );
+   }
+ }
+ 
+
+ 
+    
+    
